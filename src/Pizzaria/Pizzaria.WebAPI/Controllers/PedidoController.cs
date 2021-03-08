26f1fe50a36c85bf265e89b2ec76da5399c64f62
Anderson Souza <anderson.silveira@confitec.com.br>
2021-03-07 23:24:46 -0300

    Adicionando outras controllers

---
+++ src/Pizzaria/Pizzaria.WebAPI/Controllers/PedidoController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pizzaria.Core.API.Controller;
+
+namespace Pizzaria.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PedidoController : BaseController
+    {
+        public PedidoController([FromServices] IMediator mediator)
+            : base(mediator)
+        {
+        }
+    }
+}
---
+++ src/Pizzaria/Pizzaria.WebAPI/Controllers/ProdutoController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Pizzaria.Core.API.Controller;
+
+namespace Pizzaria.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProdutoController : BaseController
+    {
+        public ProdutoController([FromServices] IMediator mediator)
+            : base(mediator)
+        {
+        }
+    }
+}
