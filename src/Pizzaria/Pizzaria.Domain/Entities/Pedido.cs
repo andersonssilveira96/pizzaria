1a11682b8cd80d580d49548d1d351f9baa827e1a
Anderson Souza <anderson.silveira@confitec.com.br>
2021-03-30 00:17:45 -0300

    Modelando pedido

--- src/Pizzaria/Pizzaria.Domain/Entities/Pedido.cs
+++ src/Pizzaria/Pizzaria.Domain/Entities/Pedido.cs
@@ -5,14 +5,27 @@ namespace Pizzaria.Domain.Entities
 {
     public class Pedido : EntityBase
     {
-        public Pedido()
+        public Pedido(decimal valorTotal, decimal desconto, decimal frete, int statusPedidoId, IEnumerable<Produto> produto, int clienteId)
+        {
+            ValorTotal = valorTotal;
+            Desconto = desconto;
+            Frete = frete;
+            StatusPedidoId = statusPedidoId;
+            Produto = produto;
+            ClienteId = clienteId;
+        }
+        protected Pedido()
         {
         }      
+
+
         public decimal ValorTotal { get; private set; }
         public decimal Desconto { get; private set; }
         public decimal Frete { get; private set; }
-        public int Status { get; set; }
+        public int StatusPedidoId { get; private set; }
+        public virtual StatusPedido StatusPedido { get; private set; }
         public virtual IEnumerable<Produto> Produto { get; private set; }
+        public int ClienteId { get; private set; }
         public virtual Cliente Cliente { get; private set; }
     }    
 }
